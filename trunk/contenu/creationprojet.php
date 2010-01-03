d162f67c87a90fa2cdae0ec1916d06747de74c35
synaphis <synaphis@bbeda306-d844-11de-90a4-759a056a3192>
2010-01-03 22:23:02 +0000

    fix creationprojet & loader

--- trunk/contenu/creationprojet.php
+++ trunk/contenu/creationprojet.php
@@ -3,29 +3,11 @@ if (isset($_SESSION["ses_connecte"]))
 {
 ?>
 
-<?php
-if (isset($_POST["nom"]) & isset($_POST["description"]))
-{
-	date_default_timezone_set('Europe/Paris');
-		
-	$nom = mysql_real_escape_string(htmlspecialchars($_POST["nom"]));
-	$description = mysql_real_escape_string(htmlspecialchars($_POST["description"]));
-	mysql_query("INSERT INTO projets(nom,date,description) VALUES('$nom', CURDATE(), '$description')") or die("INSERT INTO projets : ".mysql_error());
-	
-	$q = mysql_query("SELECT id FROM projets WHERE description='$description' AND nom='$nom'") or die("SELECT FROM projets : ".mysql_error());
-	$r = mysql_fetch_array($q);
-	$id_projet_cree = $r["id"];
-	$id = $_SESSION['ses_id'];
-	mysql_query("INSERT INTO participer VALUES ('".$id."', '".$id_projet_cree."')") or die("Insertion dans Participer : ".mysql_error());
 
-	echo "Projet ajoute";
-
-}
-?>
 
 <h3>Creation d'un projet</h3>
-<form method="post" action="./creationprojet.php">
-
+<form method="post" action="projet.php">
+<input type="hidden" name="creationprojet" value="1" />
 <table>
 		<tr><td><label>Nom du projet : </label></td><td><input type="text" id="nom" name="nom" /></td></tr>
 		<tr><td><label>Description : </label></td><td><input type="text" id="description" name="description" /></td></tr>
