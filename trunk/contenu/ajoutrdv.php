3519c9624788fcc2004912fafd5634aeaa38fd5b
synaphis <synaphis@bbeda306-d844-11de-90a4-759a056a3192>
2010-01-03 19:18:54 +0000

    Correction affichage RDV et W3C-valide.

--- trunk/contenu/ajoutrdv.php
+++ trunk/contenu/ajoutrdv.php
@@ -38,7 +38,8 @@ else
 <?php
 	date_default_timezone_set('Europe/Paris');
 	$hauteur_jour = 20;
-	$largeur_gauche = 70;
+	$hauteur_heure = 42;
+	$largeur_gauche = 42;
 	$largeur_jour = 120;
 	
 ?>
@@ -55,46 +56,50 @@ else
 </div>
 
 
-<div style="border:1px solid black; width: <?php echo $largeur_gauche + $largeur_jour*7 + 5; ?>px; height: 500px; overflow:scroll; position: relative;">
+<div style="border:1px solid black; height: 500px; overflow-y:scroll; position: relative;">
 <?php
 
 
-$minutes = 0; $heures = 0;
-for ($i=0; $i < 48; $i++)
+//$minutes = 0; $heures = 0;
+for ($i=0; $i < 24; $i++)
 {
+/*
 	if ($minutes == 60)
 	{
 		$heures = $heures + 1;
 		$minutes = 0;
 	}
+	*/
 	?>
-	<div style="position:absolute; top:<?php echo $hauteur_jour + $i*30; ?>px; width:<?php echo $largeur_gauche + $largeur_jour*7; ?>px; border-top : 1px solid #D9D9D9;">
-		<span style="position:absolute;"><?php echo $heures . "h " . $minutes; ?></span>
-	</div>
+	<div style="position:absolute; top:<?php echo $i*$hauteur_heure; ?>px; width:<?php echo $largeur_gauche + $largeur_jour*7; ?>px; border-top : 1px solid #D9D9D9;"></div>
+	<div style="position:absolute; top:<?php echo $i*$hauteur_heure+$hauteur_heure/2; ?>px; width:<?php echo $largeur_gauche + $largeur_jour*7; ?>px; border-top : 1px dotted #D9D9D9;"></div>
+	
+	
+	<span style="position:absolute; text-align: right; width: <?php echo $largeur_gauche; ?>px; top:<?php echo $i*$hauteur_heure; ?>px;"><?php echo $i; ?>:00</span>
 	<?php
-$minutes = $minutes + 30;
+//$minutes = $minutes + 30;
 
+
+
+}
 
 	// Affichage des lignes verticales
 	for ($j=0 ; $j < 7; $j++)
 	{
 		?>
-		<div style="position:absolute; top:<?php echo $hauteur_jour + $i*30; ?>px; left:<?php echo $largeur_gauche + $largeur_jour*$j; ?>px; width:<?php echo $largeur_jour; ?>px; border-left : 1px solid #D9D9D9;"> .
-		</div>
+		<div style="position:absolute; height: <?php echo 24*$hauteur_heure; ?>px; top:0px; left:<?php echo $largeur_gauche + $largeur_jour*$j; ?>px; width:<?php echo $largeur_jour; ?>px; border-left : 1px solid #D9D9D9;"></div>
 		<?php
 	}	
-}
-
-
+	
 $q = mysql_query("SELECT *, DATEDIFF(date,CURDATE()) as diff FROM rdv");
 while ($d = mysql_fetch_array($q) )
 {
 	$hd = $d["heuredeb"];
 	$hf = $d["heurefin"];
-	$top = $hauteur_jour+substr($hd,0,2)*60+substr($hd,3,5);
-	$height = substr($hf,0,2)*60+substr($hf,3,5) - $top;
+	$top = (substr($hd,0,2)*60+substr($hd,3,5))/60*$hauteur_heure;
+	$height = (substr($hf,0,2)*60+substr($hf,3,5))/60*$hauteur_heure - $top;
 	$left = $largeur_gauche + $d["diff"]*$largeur_jour;
-	?><div style="position: absolute; top: <?php echo $top; ?>px; left: <?php echo $left; ?>px; width: 100px; height: <?php echo $height; ?>px; background-color: red;"><?php echo $d["commentaire"]; ?></div>
+	?><div class="cliquable" onclick="alert('test');" style="position: absolute; top: <?php echo $top; ?>px; left: <?php echo $left; ?>px; width: <?php echo $largeur_jour; ?>px; height: <?php echo $height; ?>px; background-color: red;"><?php echo $d["commentaire"]; ?></div>
 	<?php
 
 }
